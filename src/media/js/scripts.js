a987b40680c862016c666e93560f291547353c22
Eduardo Diaz <eduardo.diaz@movingimage24.de>
2016-01-06 17:00:19 +0100

    Navigation Bar

--- src/media/js/scripts.js
+++ src/media/js/scripts.js
@@ -3,3 +3,10 @@ var bootstrap = require('bootstrap');
 
 console.log('scripts loaded ...');
 
+$(document).ready(function () {
+    $('.banner .fa-bars').click(function () {
+        $(this).add('.navigation-wrapper').toggleClass('open');
+    });
+});
+
+
